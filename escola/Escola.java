42541a458f7758ddbb69c846ee04ec86e43a2b8e
Francisco <125330741+ExpeditoTay@users.noreply.github.com>
2023-06-07 20:52:22 -0300

    Classes Professor, Aluno, Disciplina e teste de cadastro de 5 alunos.

---
+++ escola/Escola.java
@@ -0,0 +1,31 @@
+
+package com.mycompany.escola;
+
+import com.mycompany.escola.entidades.Aluno;
+import com.mycompany.escola.entidades.Disciplina;
+import java.util.List;
+
+
+public class Escola {
+    public static void main(String[] args) {
+        //CRIAR UMA DISCIPLINA
+        
+        Disciplina disciplina1 = new Disciplina();
+        preencher(disciplina1);
+        imprimir();
+    }
+
+    public static void preencher(Disciplina disciplina1) {
+        
+        //CRIAR LISTA DE ALUNOS
+        
+        List<Aluno> lstAluno = new ArraList();
+        
+    }
+
+    public static void imprimir() {
+        
+    }
+    
+    }
+
